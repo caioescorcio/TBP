b025152c09f53e8a7ac742fec5b864dd6650d3e5
vpspepe-usp <victor101pepe@usp.br>
2024-02-24 00:06:22 -0300

    RK Vetorial

--- Classes/Discretizacao.py
+++ Classes/Discretizacao.py
@@ -5,7 +5,7 @@ class Discretizacao:
     
     def rungekutta(y_0, f, n, intervalo_t):
         #discretizacao do intervalo
-        delta_t = (intervalo_t[1] - intervalo_t[0])/n
+        h = (intervalo_t[1] - intervalo_t[0])/n
 
         #inicializacao das variaveis
         t = intervalo_t[0]
@@ -15,55 +15,76 @@ class Discretizacao:
 
         #loop para calcular os valores de y
         while t < intervalo_t[1]:
-            k_1 = delta_t*f(t, y)
-            k_2 = delta_t*f(t + delta_t/2, y + k_1/2)
-            k_3 = delta_t*f(t +delta_t/2, y + k_2/2)
-            k_4 = delta_t*f(t + delta_t, y + k_3)
+            k_1 = h*f(t, y)
+            k_2 = h*f(t + h/2, y + k_1/2)
+            k_3 = h*f(t +h/2, y + k_2/2)
+            k_4 = h*f(t + h, y + k_3)
 
             y = y + (k_1 + 2*k_2 + 2*k_3 + k_4)/6
-            t = t + delta_t
+            t = t + h
             v_t.append(t)
             v_y.append(y)
             
         return v_t, v_y
     
+    def calculaNovaVelocidade(c1, c2, c3, incremento, h, k, l, i):
+        cx = Planeta(c1.posicao + incremento*k[i], c1.massa, c1.velocidade[i] + incremento*l[i])
+        cx_1 = Planeta(c2.posicao + incremento*k[(i+1)%3], c2.massa, c2.velocidade + incremento*l[(i+1)%3])
+        cx_2 = Planeta(c3.posicao + incremento*k[(i+2)%3], c3.massa, c3.velocidade + incremento*l[(i+2)%3])
+        return cx.velocidade + h*(cx.ac_rel(c2) + cx.ac_rel(c3))
+    
     def rungekutta_planetas(corpo_1, corpo_2, corpo_3, n, T):
         #discretizacao do intervalo
-        delta_t = T/n
+        h = T/n
+        
+        #array de corpos
+        corpos = np.array([corpo_1, corpo_2, corpo_3])
 
         #definicao das funcoes para discretizar
         a1 = lambda c1, c2, c3: c1.ac_rel(c2) + c1.ac_rel(c3)
         a2 = lambda c1, c2, c3: c2.ac_rel(c1) + c2.ac_rel(c3)
         a3 = lambda c1, c2, c3: c3.ac_rel(c2) + c3.ac_rel(c1)
+        a = [a1, a2, a3]
         v = lambda c: c.velocidade
         
+        #array de termos do RK para posicao e velocidade
+        k = np.zeros(3,4)
+        l = np.zeros(3,4)
 
         #loop para calcular os valores
         while t < T:
-
-            #para as posicoes
-            k = np.zeros(4,6)
             
-            k1 = delta_t*v(corpo_1)
-            l1 = delta_t*a1(corpo_1, corpo_2, corpo_3)
-
-            k2 = delta_t*(v(corpo_1) + l1/2)
-            l2 = delta_t*a1(corpo_1 + k1/2, corpo_2, corpo_3)
-
-           
+            #K1
+            for i in range(3):
+                k[i,0] = h*v(corpos[i])
+            for i in range(3):
+                l[i,0] = h*a(corpos[i], corpos[(i+1)%3], corpos[(i+2)%3])
             
-
-
-            c1.velocidade = v1_prox
-            c2.velocidade = v2_prox
-            c3.velocidade = v3_prox 
-            c1.posicao    = r1_prox
-            c2.posicao    = r2_prox
-            c3.posicao    = r3_prox
+            #K2
+            for i in range(3):
+                k[i,1] = h*(v(corpos[i]) + (l[i,0])/2)
+            for i in range(3):
+                l[i,1] = h*calculaNovaVelocidade(corpos[i], corpos[(i+1)%3], corpos[(i+2)%3], 0.5, h, k, l, 0)
             
+            #K3
+            for i in range(3):
+                k[i,2] = h*(v(corpos[i]) + (l[i,1])/2)
+            for i in range(3):
+                l[i,2] = h*calculaNovaVelocidade(corpos[i], corpos[(i+1)%3], corpos[(i+2)%3], 0.5, h, k, l, 1)
+            
+            #K4
+            for i in range(3):
+                k[i,3] = h*(v(corpos[i]) + l[i,2])
+            for i in range(3):
+                l[i,3] = h*calculaNovaVelocidade(corpos[i], corpos[(i+1)%3], corpos[(i+2)%3], 1, h, k, l, 2)
+               
+            #Atualizando os valores    
+            for i in range(3):
+                corpos[i].posicao = corpos[i].posicao + (k[i,0] + 2*k[i,1] + 2*k[i,2] + k[i,3])/6
+                corpos[i].velocidade = corpos[i].velocidade + (l[i,0] + 2*l[i,1] + 2*l[i,2] + l[i,3])/6
+
              
-            t += delta_t
+            t += h
+            
+        return corpos[0], corpos[1], corpos[2]
 
-    def calculaNovaVelocidade(c1, c2, c3, incremento, h):
-        cx = Planeta(c1.posicao + incremento*h, c1.massa, c1.velocidade)
-        return cx.velocidade + h*(cx.ac_rel(c2) + cx.ac_rel(c3))
--- Testes/teste_aproximacao.py
+++ Testes/teste_aproximacao.py
@@ -1,5 +1,5 @@
 import numpy as np
-from Classes.Aproximacao import Aproximacao as ap
+from ..Classes.Aproximacao import Aproximacao as ap
 
 f = lambda x: (x**2 + 3)/12 #funcao teste, com uma raiz no intervalo I 
 i = [-10, 10] #intervalo i no qual pretende-se calcular o valor de f(x) = x
